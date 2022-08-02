ae9ec0c815c958f02d4419a7b2f3073c01727120
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-08-01 23:47:33 -0300

    feat: player explosion

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -6,6 +6,9 @@ public class EnemyAi : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 8.0f; 
+
+    [SerializeField]
+    private GameObject enemyExplosionAnimation;
     // Update is called once per frame
     void Update()
     {
@@ -16,24 +19,35 @@ public class EnemyAi : MonoBehaviour
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
         float randomNumber = Random.Range(-8.51f, 8.5f);
         if(transform.position.y <= -6.9f){
-            transform.position = new Vector3(randomNumber, 6.69f, 0);
+           this.transform.position = new Vector3(randomNumber, 6.69f, 0);
         }
     }
      private void OnTriggerEnter2D(Collider2D other){
-        float randomNumber = Random.Range(-8.51f, 8.5f);
- 
+
          if(other.tag == "Player"){
             Player player = other.GetComponent<Player>();
             if(player == null) return;
             player.Damage();
-            this.transform.position = new Vector3(randomNumber, 6.69f, 0);
+         
          }
          else if(other.tag == "Laser"){
             Laser laser = other.GetComponent<Laser>();
             if(laser == null) return;
-            this.transform.position = new Vector3(randomNumber, 6.69f, 0);
             Destroy(laser.gameObject);
          }
+        
+        PlayExplosionAnimation();
+        PlaceEnemyOnNewLocation();
+
+     }
+
+     private void PlaceEnemyOnNewLocation(){
+            float randomNumber = Random.Range(-8.51f, 8.5f);
+            this.transform.position = new Vector3(randomNumber, 6.69f, 0);
+     }
+
+     private void PlayExplosionAnimation() {
+            Instantiate(enemyExplosionAnimation, this.transform.position, Quaternion.identity);
      }
 
 }
