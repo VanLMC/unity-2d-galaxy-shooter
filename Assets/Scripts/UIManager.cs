325d2392c2e9d1d6803e3185a737ab85c45297a7
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-08-15 23:21:34 -0300

    feat: add canvas, uiManager and livesSprites

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public Sprite[] livesSprites;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateLives(){
+
+    }
+
+    public void UpdateScore(){
+        
+    }
+}
