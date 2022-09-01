c206838283b04da60fe4a92eba615e4ec774b471
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-08-31 22:52:54 -0300

    feat: destroy explosion after 4 seconds

---
+++ Assets/Scripts/ExplosionEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionEffect : MonoBehaviour
+{
+
+    void Start()
+    {
+        Destroy(this.gameObject, 4f);
+    }
+
+
+}
--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -12,6 +12,9 @@ public class Powerup : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if(transform.position.y <= -6.9f){
+           Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other){
